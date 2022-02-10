5fcbf186fdd6bb3f309ac1a9c0f6f966fbb72209
Taylor <97355963+TaylorFlynn@users.noreply.github.com>
2022-02-10 10:50:24 -0800

    Mobile Testing

--- app.js
+++ app.js
@@ -74,9 +74,8 @@ navLinks.forEach((Link)=> Link.addEventListener('click', hideMobileMenu));
 navLogo.addEventListener('click', hideMobileMenu);
 
 window.addEventListener('scroll', handleScroll);
-window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
+// window.addEventListener("load",function() {
+//     setTimeout(function(){
+//         window.scrollTo(0, );
+//     }, 0);
+// });
